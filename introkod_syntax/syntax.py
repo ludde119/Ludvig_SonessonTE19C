68e941be2ff8fccff9e37878b5e19a484705b819
ludde119 <70264830+ludde119@users.noreply.github.com>
2020-08-28 09:37:39 +0200

    intro_kod

---
+++ introkod_syntax/syntax.py
@@ -0,0 +1,11 @@
+print("hej kokchun")
+
+name = "ludvig"
+
+print(f"ditt namn är {name}")
+
+side = float(input("ange kvadratens sida: "))
+area = side**2
+
+print(f"arean av kvadraten är {area} a.e.")
+
